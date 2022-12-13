271931023ad36e6b9bc7c703e95d446ee157d602
edmondhabimana <edmond.habimana@yahoo.com>
2022-12-12 18:33:01 -0700

    adding padding on top on NFTs List for sale

--- pages/created-nfts.js
+++ pages/created-nfts.js
@@ -34,7 +34,7 @@ const CreatorDashboard = () => {
 
   return (
     <div className="flex justify-center sm:px-4 p-12 min-h-screen">
-      <div className="w-full minmd:w-4/5">
+      <div className="w-full minmd:w-4/5 pt-5">
         <div className="mt-4">
           <h2 className="font-poppins dark:text-white text-nft-black-1 text-2xl mt-2 ml-4 sm:ml-2 font-semibold">NFTs Listed for Sale</h2>
           <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
